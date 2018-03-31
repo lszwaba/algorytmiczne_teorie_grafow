107db9a8ef62a1d8d7a576db10380e0092d5e429
lszwaba <lszwaba@sigma.ug.edu.pl>
2018-03-31 17:12:38 +0200

    zadanie do sprawdzenia

--- graf_zajecia.py
+++ graf_zajecia.py
@@ -1,59 +1,111 @@
+
 def info():
     info = """
         Program oblicza stopnie podanych wierzcholkow oraz podaje
-    najwyzszy stopien wieszcholka. Foramta podawania danych:Przyklad
-    'A B E X T' wiercholek glowny to A (pierwszy element na liscie), reszta
-    to wierzcholki z nim polaczone.
+    stopien grafu. Foramat podawania danych:
 
-    !!!!!ABY ZAKOŃCZYĆ WPROWADZANIE DANYCH NALEZY NAPISAC 'quit'!!!!!
+    Przyklad: 'A B E X T' wiercholek glowny to A (pierwszy element na liscie),
+    reszta to wierzcholki z nim polaczone. Program modyfikuje dane tak aby
+    powstal graf prosty.
+
+    !!!!!ABY ZAKOŃCZYĆ WPROWADZANIE DANYCH NALEZY NACISNAC DWA RAZY
+    ENTER PO WPROWADZENIU DANYCH!!!!!
     """
-    print(info)
+    return print(info)
 
 
-def get_date(lista):
+def get_date():
     """
     Funkcja pobiera dane z standardowego
     wejscia i umieszcza je w liscie
     ."""
+    lista_v = []
     while True:
         get_V_and_E = input().split()
         try:
-            if get_V_and_E[0] == 'quit':
+            if get_V_and_E[0] == '':
                 break
         except IndexError:
-            print('Nie wprowadzono danych')
-        else:
-            lista.append(get_V_and_E)
+            break
+        if get_V_and_E is not '':
+            lista_v.append(get_V_and_E)
+    return lista_v
 
 
-def add_to_dict(graph, lista):
+def add_to_dict(lista):
     """
     Funkcja przenosi dane do slownika (graph). Lista[0] jest wieszcholkiem
     a lista[1:] to wierzcholki z ktorymi jest polaczony. Jeżeli wieszlek
-    jest juz w grafie dodaje do niego wierzcholki
+    jest juz w grafie dodaje do niego wierzcholki. Przyjmowane dane są
+    modyfikowane aby w wartościach poszczegulnych kluczy nie powstawaly
+    powtorzenia
     ."""
-    roboczy_dic = {}
+    graph = {}
     for date in lista:
         date_list = list(date)
         if date_list[0] in graph.keys():
-            print(graph[date_list[0]])
             lista_V = graph[date_list[0]]
             lista_V_2 = date[1:]
             lista_V.extend(lista_V_2)
-            print(type(lista_V_2))
-            roboczy_dic[date_list[0]] = lista_V
+            # nie powiela takich samych nazw wieszcholkow w value
+            graph[date_list[0]] = list(set(lista_V))
         else:
-            graph[date_list[0]] = list(date[1:])
+            graph[date_list[0]] = list(set(date[1:]))
+    return graph
 
 
-def create_graph(graph, lista):
-    get_date(lista)
-    add_to_dict(graph, lista)
-    print(graph)
+def simple_graph(graph):
+    """
+    Usuwa multikrawdzie z grafu
+    ."""
+    lis_of_key = [key for key in graph.keys()]
+    for key, value in graph.items():
+        for v in value:
+            if v in lis_of_key:
+                list_value = graph[v]
+                if key in list_value:
+                    list_value.remove(key)
+    return graph
+
+
+def create_graph():
+    return simple_graph(add_to_dict(get_date()))
+
+
+def deg(graph):
+    """
+    Funkcaja zwraca slownik z wierzcholkami i jego stopniami
+    ."""
+    deg_V = {}
+    #petla tworzy liste wszytkich wierzcholkow
+    list_V = [key for key in graph.keys()]
+    for value in graph.values():
+        list_V.extend(value)
+    #lista wierzcholkow z value slownika
+    list_value = []
+    for value in graph.values():
+        list_value.extend(value)
+    for v in list_V:
+        if v in graph.keys():
+            deg_V[v] = len(graph[v]) + list_value.count(v)
+        elif v in list_value:
+            deg_V[v] = list_value.count(v)
+    return deg_V
+
+
+def show_results(deg_V):
+    max = 0
+    while deg_V:
+        for key, value in deg_V.items():
+            print('Wierzcholek ' + key + ' jest stopnia ' + str(value))
+            if max < value:
+                max = deg_V[key]
+        print('\nStopien grafu wynosi ' + str(max))
+        break
+    print('Nie wprowadzono zadnych wierzcholkow')
 
 
 if __name__ == '__main__':
-    graph = {}
-    lista = []
     info()
-    create_graph(graph, lista)
+    g = create_graph()
+    show_results(deg(g))
--- robocze.py
+++ robocze.py
@@ -1,7 +1,47 @@
+import pprint
+b = {'1': ['30', '5', '3'],
+     '5': ['75', '1'],
+     '3': ['1', '5', '3'],
+     '4': ['1']
+     }
+la = [1, 3 ,45 ,5]
 
-while True:
-    get_V_and_E = input().split()
-    if get_V_and_E[0] is null:
-        print('nie podano wiercholkow')
-    elif get_V_and_E[0] == 'quit':
-        break
+
+def simple_graph(d):
+    """
+    Usuwa multikrawdzi oraz petle
+    ."""
+    lis_of_key = [key for key in d.keys()]
+    for key, value in d.items():
+        for v in value:
+            if v in lis_of_key:
+                list_value = d[v]
+                if key in list_value:
+                    list_value.remove(key)
+    return d
+
+
+
+def deg(graph):
+    deg_V = {}
+    #petla tworzy liste wszytkich wierzcholkow
+    list_V = [key for key in graph.keys()]
+    #lista wierzcholkow z value slownika
+    list_value = []
+    for value in graph.values():
+        list_value.extend(value)
+    for value in graph.values():
+        list_V.extend(value)
+    uniq_list_v = list(set(list_V))
+    print(list_V)
+    for v in uniq_list_v:
+        if v in graph.keys() and v in list_value:
+            deg_V[v] = len(graph[v]) + list_value.count(v)
+        elif v in list_value:
+            deg_V[v] = list_value.count(v)
+    return deg_V
+
+
+
+simple_graph(b)
+deg(simple_graph(b))
